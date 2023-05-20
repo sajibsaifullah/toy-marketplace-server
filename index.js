e148be66d771a672c827de85efaf8892aa392e18
sajibsaifullah <dusajibphy@gmail.com>
2023-05-20 19:49:16 +0600

    email query added

--- index.js
+++ index.js
@@ -26,27 +26,48 @@ async function run() {
     await client.connect();
 
     const toyCollection = client.db("toyHouse").collection("toys");
+    const myToyCollection = client.db("toyHouse").collection("myToys");
 
     app.get("/allToys", async (req, res) => {
       const result = await toyCollection.find().toArray();
       res.send(result);
     });
 
-    app.post('/allToys', async(req, res) => {
-      const allToys = req.body;
-      // console.log(allToys);
-      const result = await toyCollection.insertOne(allToys);
-      res.send({result});
-    })
-
+    // app.post('/addToys', async(req, res) => {
+    //   const allToys = req.body;
+    //   // console.log(allToys);
+    //   const result = await toyCollection.insertOne(allToys);
+    //   res.send({result});
+    // })
 
     app.get("/allToys/:text", async (req, res) => {
       // console.log(req.params.text)
-      const result = await toyCollection.find({subcategory: req.params.text}).toArray();
+      const result = await toyCollection
+        .find({ subcategory: req.params.text })
+        .toArray();
       res.send(result);
     });
 
+    // myToys
 
+    app.get("/myToys", async (req, res) => {
+      // const result = await myToyCollection.find().toArray();
+      // res.send(result);
+
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await myToyCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/myToys", async (req, res) => {
+      const toys = req.body;
+      // console.log(myToys);
+      const result = await myToyCollection.insertOne(toys);
+      res.send({ result });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
